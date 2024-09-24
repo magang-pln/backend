2a862801fc5461aa181adb1097bb422d720fb516
itsmegita <gitamarendes@gmail.com>
2024-09-24 09:30:31 +0800

    change config section

--- config/config.js
+++ config/config.js
@@ -7,8 +7,12 @@ module.exports = {
     dialect: "postgres",
     port: process.env.DB_PORT,
   },
-  production: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
+  use_env_variable: "DATABASE_URL",
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 };
