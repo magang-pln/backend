913b2b1fc85017065735a4e932334614a0d1a899
itsmegita <gitamarendes@gmail.com>
2024-09-24 09:48:32 +0800

    change authRouter and authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,15 +4,13 @@ const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 const ApiError = require("../utils/apiError");
+const { Op } = require("sequelize");
 
 const register = async (req, res, next) => {
   try {
     const { email, password, confirmPassword } = req.body;
 
-    const isUserExist = await User.findOne({
-      where: { email },
-    });
-
+    const isUserExist = await User.findOne({ where: { email } });
     if (isUserExist) {
       return next(new ApiError("Email already registered", 400));
     }
@@ -25,26 +23,17 @@ const register = async (req, res, next) => {
       return next(new ApiError("Passwords do not match", 400));
     }
 
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({
-      email,
-      password: hashedPassword,
-      confirmPassword: hashedPassword,
-    });
+    const newUser = await User.create({ email, password: hashedPassword });
 
     res.status(201).json({
       status: "Success",
       message: "User registered successfully",
-      data: {
-        id: newUser.id,
-        email: newUser.email,
-        password: hashedPassword,
-      },
+      data: { id: newUser.id, email: newUser.email },
     });
   } catch (err) {
-    next(new ApiError(err.message, 500));
+    return next(new ApiError("Failed to register user", 500));
   }
 };
 
@@ -73,96 +62,90 @@ const login = async (req, res, next) => {
     res.status(200).json({
       status: "Success",
       message: "Login successful",
-      data: token,
+      data: { token },
     });
   } catch (err) {
-    return next(new ApiError(err.message, 500));
+    return next(new ApiError("Failed to log in", 500));
   }
 };
 
-const resetPasswordRequest = async (req, res, next) => {
-  try {
-    const { email } = req.body;
+// const resetPasswordRequest = async (req, res, next) => {
+//   try {
+//     const { email } = req.body;
+//     const user = await User.findOne({ where: { email } });
 
-    const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return next(new ApiError("No user found with this email", 400));
-    }
+//     if (!user) {
+//       return next(new ApiError("No user found with this email", 400));
+//     }
 
-    const token = crypto.randomBytes(32).toString("hex");
-    const expiration = Date.now() + 900000;
+//     const token = crypto.randomBytes(32).toString("hex");
+//     const expiration = Date.now() + 900000;
 
-    await user.update({ resetToken: token, resetTokenExpiration: expiration });
+//     await user.update({ resetToken: token, resetTokenExpiration: expiration });
 
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+//     const transporter = nodemailer.createTransport({
+//       service: "Gmail",
+//       auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.EMAIL_PASSWORD,
+//       },
+//     });
 
-    const mailOptions = {
-      from: process.env.EMAIL,
-      to: user.email,
-      subject: "Password Reset",
-      text: `You requested a password reset. Use the following link to reset your password: \n\n
-             ${process.env.FRONTEND_URL}/reset-password/${token} \n\n
-             This link is valid for 15 minutes.`,
-    };
+//     const mailOptions = {
+//       from: process.env.EMAIL,
+//       to: user.email,
+//       subject: "Password Reset",
+//       text: `You requested a password reset. Use the following link to reset your password:
+//              ${process.env.FRONTEND_URL}/reset-password/${token}
+//              This link is valid for 15 minutes.`,
+//     };
 
-    await transporter.sendMail(mailOptions);
+//     await transporter.sendMail(mailOptions);
 
-    res.status(200).json({
-      status: "Success",
-      message: "Password reset email sent",
-    });
-  } catch (err) {
-    console.error("Error during reset password request:", err);
-    return next(new ApiError(err.message, 500));
-  }
-};
+//     res.status(200).json({
+//       status: "Success",
+//       message: "Password reset email sent",
+//     });
+//   } catch (err) {
+//     return next(new ApiError("Failed to send reset password email", 500));
+//   }
+// };
 
-const resetPassword = async (req, res, next) => {
-  try {
-    const { token } = req.params;
-    const { newPassword } = req.body;
+// const resetPassword = async (req, res, next) => {
+//   try {
+//     const { token } = req.params;
+//     const { newPassword } = req.body;
 
-    const user = await User.findOne({
-      where: {
-        resetToken: token,
-        resetTokenExpiration: {
-          [Op.gt]: Date.now(),
-        },
-      },
-    });
+//     const user = await User.findOne({
+//       where: {
+//         resetToken: token,
+//         resetTokenExpiration: {
+//           [Op.gt]: Date.now(),
+//         },
+//       },
+//     });
 
-    if (!user) {
-      return next(new ApiError("Invalid or expired token", 400));
-    }
+//     if (!user) {
+//       return next(new ApiError("Invalid or expired token", 400));
+//     }
 
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+//     const hashedPassword = await bcrypt.hash(newPassword, 10);
+//     await user.update({
+//       password: hashedPassword,
+//       resetToken: null,
+//       resetTokenExpiration: null,
+//     });
 
-    await user.update({
-      password: hashedPassword,
-      resetToken: null,
-      resetTokenExpiration: null,
-    });
-
-    res.status(200).json({
-      status: "Success",
-      message: "Password reset successfully",
-    });
-  } catch (err) {
-    console.error("Error during password reset:", err);
-    return next(new ApiError("Failed to reset password", 500));
-  }
-};
+//     res.status(200).json({
+//       status: "Success",
+//       message: "Password reset successfully",
+//     });
+//   } catch (err) {
+//     return next(new ApiError("Failed to reset password", 500));
+//   }
+// };
 
 module.exports = {
   register,
   login,
-  resetPasswordRequest,
-  resetPassword,
 };
