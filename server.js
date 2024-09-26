40fb9002cd6cd145bdc88ba874490f77d9b529f4
itsmegita <gitamarendes@gmail.com>
2024-09-26 13:12:00 +0800

    add proxy

--- server.js
+++ server.js
@@ -3,29 +3,27 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const apiError = require("./utils/apiError");
 const errorHandler = require("./controllers/errorController");
 const router = require("./routes");
 
 const PORT = process.env.PORT;
-
 const app = express();
 
 app.set("trust proxy", true);
 
-// Konfigurasi CORS
 const corsOptions = {
   origin: ["http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
 
-app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
-app.use(express.json());
+
 app.use(morgan("dev"));
+
+app.use(express.json());
 
 app.use(router);
 
