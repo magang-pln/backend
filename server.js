2b5d797a18ee8f6f18c2e782ab47eddde3a9a31d
itsmegita <gitamarendes@gmail.com>
2024-09-26 11:02:53 +0800

    fix code placement for CORS

--- server.js
+++ server.js
@@ -22,11 +22,10 @@ const corsOptions = {
   credentials: true,
 };
 
+app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
-
-app.options("*", cors(corsOptions));
 
 app.use(router);
 
