4be5d4b7a4214cf844b9f6d3c533ba7b8c4b6dd7
itsmegita <gitamarendes@gmail.com>
2024-10-02 14:46:42 +0800

    change something on database

--- migrations/20240923134153-create-asset.js
+++ migrations/20240923134153-create-asset.js
@@ -1,91 +1,99 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Assets', {
+    await queryInterface.createTable("Assets", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       unit_induk: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       nama_aset: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       unit: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       nomor_SAP: {
-        type: Sequelize.INTEGER
+        type: Sequelize.TEXT,
       },
       luas: {
-        type: Sequelize.INTEGER
+        type: Sequelize.FLOAT,
       },
       harga_perolehan: {
-        type: Sequelize.INTEGER
+        type: Sequelize.BIGINT,
       },
       tahun_perolehan: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       nilai_saat_ini: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING,
       },
       sumber_perolehan: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       alamat: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       kelurahan: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       kecamatan: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       kota: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       provinsi: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       nomor_sertipikat: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       tanggal_berlaku_sertipikat: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       tanggal_berakhir_sertipikat: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       penguasaan_tanah: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       permasalahan_aset: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
-      kantah_BPN_sertifiksai: {
-        type: Sequelize.STRING
+      kantah_BPN_sertifikasi: {
+        type: Sequelize.STRING,
       },
       kronologis: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       sertipikat: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+      },
+      kordinat_x: {
+        type: Sequelize.TEXT,
+      },
+      kordinat_y: {
+        type: Sequelize.TEXT,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Assets');
-  }
+    await queryInterface.dropTable("Assets");
+  },
 };
--- models/asset.js
+++ models/asset.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Asset extends Model {
     /**
@@ -13,32 +11,37 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Asset.init({
-    unit_induk: DataTypes.STRING,
-    nama_aset: DataTypes.STRING,
-    unit: DataTypes.INTEGER,
-    nomor_SAP: DataTypes.INTEGER,
-    luas: DataTypes.INTEGER,
-    harga_perolehan: DataTypes.INTEGER,
-    tahun_perolehan: DataTypes.INTEGER,
-    nilai_saat_ini: DataTypes.INTEGER,
-    sumber_perolehan: DataTypes.STRING,
-    alamat: DataTypes.TEXT,
-    kelurahan: DataTypes.STRING,
-    kecamatan: DataTypes.STRING,
-    kota: DataTypes.STRING,
-    provinsi: DataTypes.STRING,
-    nomor_sertipikat: DataTypes.TEXT,
-    tanggal_berlaku_sertipikat: DataTypes.DATE,
-    tanggal_berakhir_sertipikat: DataTypes.DATE,
-    penguasaan_tanah: DataTypes.STRING,
-    permasalahan_aset: DataTypes.STRING,
-    kantah_BPN_sertifiksai: DataTypes.STRING,
-    kronologis: DataTypes.TEXT,
-    sertipikat: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Asset',
-  });
+  Asset.init(
+    {
+      unit_induk: DataTypes.STRING,
+      nama_aset: DataTypes.STRING,
+      unit: DataTypes.INTEGER,
+      nomor_SAP: DataTypes.INTEGER,
+      luas: DataTypes.INTEGER,
+      harga_perolehan: DataTypes.INTEGER,
+      tahun_perolehan: DataTypes.INTEGER,
+      nilai_saat_ini: DataTypes.INTEGER,
+      sumber_perolehan: DataTypes.STRING,
+      alamat: DataTypes.TEXT,
+      kelurahan: DataTypes.STRING,
+      kecamatan: DataTypes.STRING,
+      kota: DataTypes.STRING,
+      provinsi: DataTypes.STRING,
+      nomor_sertipikat: DataTypes.TEXT,
+      tanggal_berlaku_sertipikat: DataTypes.DATE,
+      tanggal_berakhir_sertipikat: DataTypes.DATE,
+      penguasaan_tanah: DataTypes.STRING,
+      permasalahan_aset: DataTypes.STRING,
+      kantah_BPN_sertifikasi: DataTypes.STRING,
+      kronologis: DataTypes.TEXT,
+      sertipikat: DataTypes.TEXT,
+      kordinat_x: DataTypes.TEXT,
+      kordinat_y: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Asset",
+    }
+  );
   return Asset;
 };
